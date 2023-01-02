f89ba42ae3276714083b3ba4a9abcf7f05a917e0
Dev Choudhary <93388817+920DEV@users.noreply.github.com>
2023-01-02 23:25:20 +0530

    Modified Css and Sticky Navbar

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
-
+const sticky_header=document.querySelector(".header");
 const toggleNavbar = () => {
   // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
+
+
+
+
 mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+
+
+
+const sticky_navbar=() =>{
+  sticky_header.classList.toggle("sticky",window.scrollY>0);
+}
+window.addEventListener("scroll",() =>sticky_navbar());
+
+
+
+// window.addEventListener("scroll",function(){
+//   let header =document.querySelector(header);
+//   header=ClassList.toggle("sticky",window.scrollY>0);
+// })
