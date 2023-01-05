57cb236fb18257ecc4536923e16b829324696b88
Dev Choudhary <93388817+920DEV@users.noreply.github.com>
2023-01-05 12:34:52 +0530

    fetched Api & Modified Search Option

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 const sticky_header=document.querySelector(".header");
+const container=document.querySelector(".container1");
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };
@@ -9,36 +10,39 @@ const toggleNavbar = () => {
 
 
 
-mobile_nav.addEventListener("click", () => toggleNavbar());
+if (mobile_nav.addEventListener("click", () => toggleNavbar())) {
+  container.classList.toggle("expand");
+}
+
 
 // script of sticky navbar
-
 const sticky_navbar=() =>{
   sticky_header.classList.toggle("sticky",window.scrollY>0);
 }
 window.addEventListener("scroll",() =>sticky_navbar());
 
-// showing time
 
+
+// showing time
   timel=document.getElementById('time');
   datel=document.getElementById('date');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
-setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
-    const minutes = time.getMinutes();
-    const seconds=time.getSeconds();
-    const ampm = hour >=12 ? 'PM' : 'AM'
+// setInterval(() => {
+//     const time = new Date();
+//     const month = time.getMonth();
+//     const date = time.getDate();
+//     const day = time.getDay();
+//     const hour = time.getHours();
+//     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+//     const minutes = time.getMinutes();
+//     const seconds=time.getSeconds();
+//     const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timel.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+':'+(seconds<10?'0'+seconds:seconds)+ ' ' + `<span id="am-pm" style="font-size:2rem">${ampm}</span>`
+//     timel.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+':'+(seconds<10?'0'+seconds:seconds)+ ' ' + `<span id="am-pm" style="font-size:2rem">${ampm}</span>`
 
-    datel.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+//     datel.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
 
-}, 1000);
+// }, 1000);
--- script_api_fetch.js
+++ script_api_fetch.js
@@ -1,12 +1,58 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ccda286950msh7e32e2e882e284cp1bfa62jsn849dc874e237',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+// http://api.weatherapi.com/v1/current.json?key=f9b1eccd8bb34a02baf173439230401&q=New delhi&aqi=no
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Himachal Pradesh', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
+
+
+let location_name=document.querySelector(".city");
+let timel=document.querySelector(".date-time-info");
+let temp=document.querySelector(".temperature");
+let humid=document.querySelector(".humidity");
+let cond=document.querySelector(".condition");
+
+const t_zone=document.querySelector(".timezone");
+const search_area=document.querySelector(".find_location");
+const form =document.querySelector('form');
+
+ 
+let weather_location="New Delhi";
+
+const fetch_weather_api= async(weather_location)=>{
+	let url=`https://api.weatherapi.com/v1/current.json?key=f9b1eccd8bb34a02baf173439230401&q=${weather_location}&aqi=no`
+
+	const fetch_api =await fetch(url);
+
+	const data = await fetch_api.json();
+	console.log(data);
+	let c_ountry=data.location.country;
+	let S_name= data.location.region;
+	let local_time =data.location.localtime;
+	let time_zone= data.location.tz_id;
+	let cond_ition=data.current.condition.text;
+	let hum=data.current.humidity;
+	let temper= data.current.temp_c;
+	locationDetails(c_ountry,S_name,local_time,time_zone,cond_ition,hum,temper)
+};
+fetch_weather_api(weather_location);
+
+// function for search value;
+function searchlocation(e){
+	e.preventDefault();
+
+	weather_location = search_area.value;
+	fetch_weather_api(weather_location);
+	 
+}
+// function calling for submitting the given locaiton
+
+form.addEventListener("submit",searchlocation);
+
+// function for populating the data of given location
+
+function locationDetails(c_ountry,S_name,local_time,time_zone,cond_ition,hum,temper){
+	location_name.innerHTML=weather_location;
+	cond.innerHTML=cond_ition;
+	temp.innerHTML=temper;
+	humid.innerHTML=hum;
+	timel.innerHTML=local_time;
+	t_zone.innerHTML=time_zone;
+}
