80dfc24c97adee4163fbc106ddac08cf30793408
Dev Choudhary <93388817+920DEV@users.noreply.github.com>
2023-01-05 22:35:12 +0530

    Modified fontpage

--- script_api_fetch.js
+++ script_api_fetch.js
@@ -1,8 +1,6 @@
 // http://api.weatherapi.com/v1/current.json?key=f9b1eccd8bb34a02baf173439230401&q=New delhi&aqi=no
 
 
-
-
 let location_name=document.querySelector(".city");
 let timel=document.querySelector(".date-time-info");
 let temp=document.querySelector(".temperature");
@@ -13,7 +11,7 @@ const t_zone=document.querySelector(".timezone");
 const search_area=document.querySelector(".find_location");
 const form =document.querySelector('form');
 
- 
+	
 let weather_location="New Delhi";
 
 const fetch_weather_api= async(weather_location)=>{
@@ -24,13 +22,13 @@ const fetch_weather_api= async(weather_location)=>{
 	const data = await fetch_api.json();
 	console.log(data);
 	let c_ountry=data.location.country;
-	let S_name= data.location.region;
+	let State_name= data.location.region;
 	let local_time =data.location.localtime;
 	let time_zone= data.location.tz_id;
 	let cond_ition=data.current.condition.text;
 	let hum=data.current.humidity;
 	let temper= data.current.temp_c;
-	locationDetails(c_ountry,S_name,local_time,time_zone,cond_ition,hum,temper)
+	locationDetails(c_ountry,State_name,local_time,time_zone,cond_ition,hum,temper)
 };
 fetch_weather_api(weather_location);
 
@@ -48,7 +46,7 @@ form.addEventListener("submit",searchlocation);
 
 // function for populating the data of given location
 
-function locationDetails(c_ountry,S_name,local_time,time_zone,cond_ition,hum,temper){
+function locationDetails(c_ountry,State_name,local_time,time_zone,cond_ition,hum,temper){
 	location_name.innerHTML=weather_location;
 	cond.innerHTML=cond_ition;
 	temp.innerHTML=temper;
